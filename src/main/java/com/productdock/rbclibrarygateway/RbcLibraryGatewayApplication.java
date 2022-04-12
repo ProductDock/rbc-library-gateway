91c113eccbfea435f30c360a118a8da68be9812f
nenadbecanovicpd <nenad.becanovic@productdock.com>
2022-04-12 09:15:42 +0200

    Initial project setup

---
+++ src/main/java/com/productdock/rbclibrarygateway/RbcLibraryGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.productdock.rbclibrarygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbcLibraryGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbcLibraryGatewayApplication.class, args);
+	}
+
+}
