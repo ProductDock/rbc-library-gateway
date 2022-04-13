951c201511df249e29e0f5ebbfd068417a0e2d13
nenadbecanovicpd <nenad.becanovic@productdock.com>
2022-04-13 15:38:59 +0200

    Configure cors

--- src/main/java/com/productdock/library/gateway/config/SecurityConfig.java
+++ src/main/java/com/productdock/library/gateway/config/SecurityConfig.java
@@ -4,18 +4,36 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebFluxSecurity
 public class SecurityConfig {
     @Bean
     SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) throws Exception {
-        http.authorizeExchange()
+        http.csrf().disable()
+                .authorizeExchange()
                 .pathMatchers("/**").authenticated()
                 .anyExchange().authenticated()
-                .and()
-                .cors().and()
+                .and().cors(withDefaults())
                 .oauth2ResourceServer()
                 .jwt();
         return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedHeader("*");
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "PUT", "PATCH", "HEAD"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
