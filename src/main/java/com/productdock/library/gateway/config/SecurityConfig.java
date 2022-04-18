7288218a4472c683774c459f38fcfd6952e9ba28
NenadBecanovicPD <100684660+NenadBecanovicPD@users.noreply.github.com>
2022-04-18 15:34:31 +0200

    Configure CORS and setup gateway for catalog service (#2)

---
+++ src/main/java/com/productdock/library/gateway/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.productdock.library.gateway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import static java.util.Arrays.asList;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Value("${cors.allowed.origins}")
+    private String corsAllowedOrigins;
+
+    @Bean
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeExchange()
+                .pathMatchers("/**").authenticated()
+                .anyExchange().authenticated()
+                .and().cors(withDefaults())
+                .oauth2ResourceServer()
+                .jwt();
+        return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedOrigin(corsAllowedOrigins);
+        configuration.setAllowedMethods(asList("GET", "POST", "OPTIONS", "PUT", "PATCH", "HEAD"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
---
+++ src/test/java/com/productdock/library/gateway/config/SecurityConfigTest.java
@@ -0,0 +1,40 @@
+package com.productdock.library.gateway.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@AutoConfigureWireMock(port = 8081)
+public class SecurityConfigTest {
+
+    @Autowired
+    private WebTestClient webClient;
+
+    @Test
+    public void givenUnauthenticated_thenUnauthorizedResponse() throws Exception {
+        webClient
+                .get().uri("/api/books")
+                .exchange()
+                .expectStatus().isUnauthorized();
+    }
+
+    @Test
+    @WithMockUser
+    public void givenAuthenticated_thenStatusOkResponse() throws Exception {
+        stubFor(get(urlEqualTo("/api/books"))
+                .willReturn(aResponse()
+                        .withStatus(200)));
+
+        webClient
+                .get().uri("/api/books")
+                .exchange()
+                .expectStatus().isOk();
+    }
+
+}
